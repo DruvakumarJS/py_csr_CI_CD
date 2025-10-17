9b79eac86db9a8b9692951a21fc36b42c0e7d96c
druva <druva@netiapps.com>
2025-10-17 18:11:33 +0530

    modified conftest

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,25 +1,16 @@
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.by import By
-import time
 import pytest
 from selenium import webdriver
 
-# Opening the browser
-opt = webdriver.ChromeOptions()
-opt.add_experimental_option("detach", True)
-opt.add_argument("--disable-notifications")
-
-
-# Opening the website
 @pytest.fixture(scope="session")
 def _drivers():
+    opt = webdriver.ChromeOptions()
+    opt.add_experimental_option("detach", True)
+    opt.add_argument("--disable-notifications")
+
     driver = webdriver.Chrome(options=opt)
     driver.get("https://csr.netiapps.com/login")
-   # driver.get("http://127.0.0.1:8000/login")
     driver.maximize_window()
     driver.implicitly_wait(20)
+
     yield driver
     driver.quit()
-
